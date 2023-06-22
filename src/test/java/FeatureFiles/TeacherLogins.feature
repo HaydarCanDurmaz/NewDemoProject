f766aca410ad0eeb12d57fa54bc5ac8b985169db
HaydarCanDurmaz <candrhydr@hotmail.com>
2023-06-22 17:18:33 +0300

    POM cucumber - testng

---
+++ src/test/java/FeatureFiles/TeacherLogins.feature
@@ -0,0 +1,6 @@
+Feature: Login Functionality
+
+  Scenario:  Teacher Login with valid username and password
+    Given Navigate to Campus
+    When  Teachers Enter username and password
+    Then  User should login succesfully
---
+++ src/test/java/StepDefinations/LoginsSteps.java
@@ -0,0 +1,19 @@
+package StepDefinations;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginsSteps {
+    @Given("Navigate to Campus")
+    public void navigateToCampus() {
+    }
+
+    @When("Teachers Enter username and password")
+    public void teachersEnterUsernameAndPassword() {
+    }
+
+    @Then("User should login succesfully")
+    public void userShouldLoginSuccesfully() {
+    }
+}
